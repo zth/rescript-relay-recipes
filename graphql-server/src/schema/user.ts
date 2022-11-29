96c55da27d73916dfe99bcebfe656d08bc7f45c1
mbirkegaard <mbirkegaard@gmail.com>
2022-11-29 15:02:41 +0100

    Add mutation to change username

--- graphql-server/src/schema/user.ts
+++ graphql-server/src/schema/user.ts
@@ -1,4 +1,5 @@
-import { builder } from './builder'
+import { builder, prisma } from './builder'
+import { ErrorInterface } from './error'
 
 export const User = builder.prismaNode('User', {
   id: {
@@ -8,3 +9,48 @@ export const User = builder.prismaNode('User', {
     username: t.exposeString('username'),
   }),
 })
+
+class ChangeUsernameError extends Error {}
+builder.objectType(ChangeUsernameError, {
+  name: 'ChangeUsernameError',
+  interfaces: [ErrorInterface],
+  fields: t => ({
+    message: t.exposeString('message'),
+  }),
+})
+
+builder.mutationFields(t => ({
+  changeUsername: t.field({
+    type: User,
+    errors: {
+      union: {
+        name: 'ChangeUsernameResult',
+      },
+      result: {
+        name: 'UsernameChanged',
+      },
+      dataField: {
+        name: 'user',
+      },
+      types: [ChangeUsernameError],
+    },
+    args: {
+      username: t.arg.string({ required: true }),
+    },
+    authScopes: {
+      authenticated: true,
+    },
+    resolve: async (_, { username }, { session }) => {
+      if (session.type === 'Authenticated') {
+        return prisma.user.update({
+          where: {
+            id: session.userId,
+          },
+          data: {
+            username,
+          },
+        })
+      }
+    },
+  }),
+}))
